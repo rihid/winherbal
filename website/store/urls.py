ee28c6510208d87b4082fd92352b1c26890e2dc1
rizal-haw <rizalhidayat006@gmail.com>
2022-09-05 13:49:51 +0700

    update

--- website/store/urls.py
+++ website/store/urls.py
@@ -7,7 +7,7 @@ app_name = 'store'
 urlpatterns = [
     path('', views.home, name='home'),
     path('product/', views.product_all, name='product_all'),
-    path('tentang/', views.about, name='about'),
+    path('tentang/', views.about, name='tentang'),
     path('product/<slug:slug>', views.product_detail, name='product_detail'),
     path('product/detail/<slug:category_slug>/', views.category_list, name='category_list'),
 ]
